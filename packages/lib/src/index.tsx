cccd4dd0cf6695e80cc41495da1aec23b65da8ad
Somi Park <youyoy689@gmail.com>
2024-09-05 19:03:15 +0900

    feat: 무한 루프가 발생했을 때, 로컬스토리지에 key가 이미 있을 때 사용자에게 콘솔로 경고메시지를 보여주는 기능 구현 (#1)

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,30 +1,47 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function useLocalstorageQuery<T = undefined>(
   key: string,
   initialValue?: T | null
 ) {
   const [data, setData] = useState<T | null>();
+  const isFirstRender = useRef(true);
+  const prevStringifiedInitialValue = useRef('');
 
   const stringifiedInitialValue =
     initialValue !== undefined
       ? JSON.stringify(initialValue)
       : JSON.stringify('undefined');
 
+  warnInfiniteLoop(
+    isFirstRender.current,
+    prevStringifiedInitialValue.current,
+    stringifiedInitialValue
+  );
+
+  prevStringifiedInitialValue.current = stringifiedInitialValue;
+  isFirstRender.current = false;
+
+  const value = localStorage.getItem(key);
+
+  const notNullValueAndNotUndefined =
+    value !== null && value !== JSON.stringify('undefined');
+
+  const notNullValueButUndefined =
+    value !== null && value === JSON.stringify('undefined');
+
+  warnIfLocalStorageKeyExists(
+    notNullValueAndNotUndefined,
+    notNullValueButUndefined,
+    initialValue
+  );
+
   useEffect(() => {
-    const value = localStorage.getItem(key);
-
-    const notNullValueAndNotUndefined =
-      value !== null && value !== JSON.stringify('undefined');
-
     if (notNullValueAndNotUndefined) {
       const parsed = JSON.parse(value);
       setData(parsed);
       return;
     }
-
-    const notNullValueButUndefined =
-      value !== null && value === JSON.stringify('undefined');
 
     if (notNullValueButUndefined) {
       return;
@@ -37,7 +54,13 @@ export default function useLocalstorageQuery<T = undefined>(
     }
 
     localStorage.setItem(key, JSON.stringify('undefined'));
-  }, [key, stringifiedInitialValue]);
+  }, [
+    key,
+    notNullValueAndNotUndefined,
+    notNullValueButUndefined,
+    stringifiedInitialValue,
+    value,
+  ]);
 
   const mutate = (newValue: T) => {
     const value = localStorage.getItem(key);
@@ -66,4 +89,38 @@ export default function useLocalstorageQuery<T = undefined>(
     mutate,
     remove,
   };
+}
+
+function warning(message: string) {
+  console.warn(message);
+}
+
+function warnInfiniteLoop(
+  isFirstRender: boolean,
+  prevStringifiedInitialValue: string,
+  stringifiedInitialValue: string
+) {
+  if (
+    !isFirstRender &&
+    prevStringifiedInitialValue !== stringifiedInitialValue
+  ) {
+    warning(
+      '들어오는 initialValue가 매번 달라지므로 무한 리렌더링에 빠질 수 있습니다.'
+    );
+  }
+}
+
+function warnIfLocalStorageKeyExists<T>(
+  notNullValueAndNotUndefined: boolean,
+  notNullValueButUndefined: boolean,
+  initialValue?: T | null
+) {
+  if (
+    initialValue &&
+    (notNullValueAndNotUndefined || notNullValueButUndefined)
+  ) {
+    warning(
+      '해당 key에 대한 값이 로컬 스토리지에 이미 설정되어있으므로, 원하는 initialValue로 초기화되지 않았을수도 있습니다. 원하지 않는 동작이라면 로컬 스토리지에서 해당 key에 대한 값을 삭제하세요.'
+    );
+  }
 }
