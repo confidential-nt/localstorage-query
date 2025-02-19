d0082290e4a5f35935eb4a933fb0b1b4dce091b6
confidential-nt <youyoy689@gmail.com>
2025-02-19 20:37:59 +0900

    feat: add some feat

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function useLocalstorageQuery<T = undefined>(
   key: string,
@@ -6,29 +6,29 @@ export default function useLocalstorageQuery<T = undefined>(
 ) {
   const [data, setData] = useState<T | null>();
   const isFirstRender = useRef(true);
-  const prevStringifiedInitialValue = useRef('');
+  const prevStringifiedInitialValue = useRef("");
 
   const stringifiedInitialValue =
     initialValue !== undefined
       ? JSON.stringify(initialValue)
-      : JSON.stringify('undefined');
+      : JSON.stringify("undefined");
 
   warnInfiniteLoop(
     isFirstRender.current,
     prevStringifiedInitialValue.current,
     stringifiedInitialValue
   );
-
+  console.log("add some feature");
   prevStringifiedInitialValue.current = stringifiedInitialValue;
   isFirstRender.current = false;
 
   const value = localStorage.getItem(key);
 
   const notNullValueAndNotUndefined =
-    value !== null && value !== JSON.stringify('undefined');
+    value !== null && value !== JSON.stringify("undefined");
 
   const notNullValueButUndefined =
-    value !== null && value === JSON.stringify('undefined');
+    value !== null && value === JSON.stringify("undefined");
 
   const deleted = value === null && data === null;
 
@@ -53,13 +53,13 @@ export default function useLocalstorageQuery<T = undefined>(
       return;
     }
 
-    if (stringifiedInitialValue !== JSON.stringify('undefined')) {
+    if (stringifiedInitialValue !== JSON.stringify("undefined")) {
       localStorage.setItem(key, stringifiedInitialValue);
       setData(JSON.parse(stringifiedInitialValue));
       return;
     }
 
-    localStorage.setItem(key, JSON.stringify('undefined'));
+    localStorage.setItem(key, JSON.stringify("undefined"));
   }, [
     key,
     notNullValueAndNotUndefined,
@@ -75,7 +75,7 @@ export default function useLocalstorageQuery<T = undefined>(
 
       const stringifiedNewValue =
         newValue === undefined
-          ? JSON.stringify('undefined')
+          ? JSON.stringify("undefined")
           : JSON.stringify(newValue);
 
       if (value !== null) {
@@ -115,7 +115,7 @@ function warnInfiniteLoop(
     prevStringifiedInitialValue !== stringifiedInitialValue
   ) {
     warning(
-      '들어오는 initialValue가 매번 달라지므로 무한 리렌더링에 빠질 수 있습니다.'
+      "들어오는 initialValue가 매번 달라지므로 무한 리렌더링에 빠질 수 있습니다."
     );
   }
 }
@@ -130,7 +130,7 @@ function warnIfLocalStorageKeyExists<T>(
     (notNullValueAndNotUndefined || notNullValueButUndefined)
   ) {
     warning(
-      '해당 key에 대한 값이 로컬 스토리지에 이미 설정되어있으므로, 원하는 initialValue로 초기화되지 않았을수도 있습니다. 원하지 않는 동작이라면 로컬 스토리지에서 해당 key에 대한 값을 삭제하세요.'
+      "해당 key에 대한 값이 로컬 스토리지에 이미 설정되어있으므로, 원하는 initialValue로 초기화되지 않았을수도 있습니다. 원하지 않는 동작이라면 로컬 스토리지에서 해당 key에 대한 값을 삭제하세요."
     );
   }
 }
